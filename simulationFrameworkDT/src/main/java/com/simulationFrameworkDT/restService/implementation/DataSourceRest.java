aecff3c38df3401f47a16446de531ec47dff37ba
NicolasB21 <nicolas.biojo@correo.icesi.edu.co>
2020-09-19 16:06:11 -0500

    fixed rest service

--- simulationFrameworkDT/src/main/java/com/simulationFrameworkDT/restService/implementation/DataSourceRest.java
+++ simulationFrameworkDT/src/main/java/com/simulationFrameworkDT/restService/implementation/DataSourceRest.java
@@ -7,12 +7,12 @@ import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.simulationFrameworkDT.dataSource.DataSourceSystem;
 import com.simulationFrameworkDT.restService.interfaces.IDataSourceRest;
-import com.simulationFrameworkDT.simulation.SimController;
 import com.simulationFrameworkDT.systemState.factorySITM.SITMCalendar;
 import com.simulationFrameworkDT.systemState.factorySITM.SITMLine;
 import com.simulationFrameworkDT.systemState.factorySITM.SITMPlanVersion;
@@ -26,20 +26,12 @@ public class DataSourceRest implements IDataSourceRest{
 	@Autowired
 	private DataSourceSystem dataSource;
 	
-	@Autowired
-	private SimController sim;
-	
-	@GetMapping("/test")
-	public ArrayList<SITMPlanVersion> findPlanVersions() {
-		return sim.getDataSource().findAllPlanVersions();
-	}
-	
 	@GetMapping("/datagrams")
 	public String[] getFileNames() {
 		return dataSource.getFileNames();
 	}
 	
-	@GetMapping("/settype")
+	@PutMapping("/settype")
 	public void setSourceType(String type) {
 		dataSource.setType(type);
 	}
