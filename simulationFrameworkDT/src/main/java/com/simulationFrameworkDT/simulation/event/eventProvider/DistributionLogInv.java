d0c91d1674166b20d678421d02510d517ce5d580
NicolasB2 <nicolas.biojo@correo.icesi.edu.co>
2020-11-15 19:17:39 -0500

    firs simulation approach

--- simulationFrameworkDT/src/main/java/com/simulationFrameworkDT/simulation/event/eventProvider/DistributionLogInv.java
+++ simulationFrameworkDT/src/main/java/com/simulationFrameworkDT/simulation/event/eventProvider/DistributionLogInv.java
@@ -5,6 +5,10 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import com.simulationFrameworkDT.model.factorySITM.SITMBus;
 
 import rcaller.RCaller;
 
@@ -18,10 +22,39 @@ public class DistributionLogInv {
 		Date init = new Date(dateFormat.parse("2019-06-20 18:00:00").getTime());
 		Date end = new Date(dateFormat.parse("2019-06-20 18:30:00").getTime());
 		
+		Queue<SITMBus> queueBus = new LinkedList<SITMBus>();
+		Queue<Long> queueServiceTime = new LinkedList<Long>();
+		
+		int interArrivalTime = 0;
+		int service = 0;
+		
 		while(init.getTime() < end.getTime()) {
-			int value = (int) getValue(415.5, 362.29);
-			init = new Date(init.getTime() + (value*1000));
-			System.out.println(init.toString());
+			
+			interArrivalTime = (int) getValue(415.5, 362.29);
+			long actualTime = init.getTime() + (interArrivalTime*1000);
+			
+			service = (int) getValue(92.95, 352.19);
+			long leaveTime = actualTime + (service*1000);
+			
+			
+			if(!queueServiceTime.isEmpty() && queueServiceTime.peek()<actualTime) {
+				Date date = new Date(queueServiceTime.peek()); 
+				queueBus.poll();
+				queueServiceTime.poll();
+				System.err.println(date.toString()+" Buses"+queueBus.size());
+			}
+			
+			queueBus.offer(new SITMBus());
+			queueServiceTime.offer(leaveTime);
+			init = new Date(actualTime);
+			System.out.println(init.toString()+" Buses"+queueBus.size());
+		}
+		
+		while(!queueBus.isEmpty()) {
+			Date date = new Date(queueServiceTime.peek()); 
+			queueBus.poll();
+			queueServiceTime.poll();
+			System.err.println(date.toString()+" Buses"+queueBus.size());
 		}
 		
 	}
