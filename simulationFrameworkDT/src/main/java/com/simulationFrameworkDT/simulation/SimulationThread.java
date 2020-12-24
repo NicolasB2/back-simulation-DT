4cd01827f7ba092365622aaede2aaaf412bbeb37
NicolasB2 <nicolas.biojo@correo.icesi.edu.co>
2020-12-24 14:00:54 -0500

    update

--- simulationFrameworkDT/src/main/java/com/simulationFrameworkDT/simulation/SimulationThread.java
+++ simulationFrameworkDT/src/main/java/com/simulationFrameworkDT/simulation/SimulationThread.java
@@ -10,7 +10,6 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 import com.simulationFrameworkDT.model.SimulationEvent;
-import com.simulationFrameworkDT.simulation.event.Event;
 import com.simulationFrameworkDT.simulation.event.eventProvider.EventGenerator;
 import com.simulationFrameworkDT.simulation.state.Project;
 import com.simulationFrameworkDT.simulation.tools.ProbabilisticDistribution;
@@ -43,8 +42,22 @@ public class SimulationThread extends Thread {
 		this.project = project;
 		this.eventGenerator = new EventGenerator();
 	}
+	
+	public void test() {
+//		Date passengerArrivetime = passengersTime.poll();
+//		int numPassengersPerBus = 0;
+//		
+//		while(!passengersTime.isEmpty() && passengerArrivetime.getTime()<=leave.getDate().getTime()) {
+//			numPassengersPerBus ++;
+//			long Hobsp = (leave.getDate().getTime() - passengerArrivetime.getTime())/1000;
+//			Hobsps.add(Hobsp);
+//			passengerArrivetime = passengersTime.poll();
+//		}
+//		
+//		System.out.println("Passengers per Bus "+numPassengersPerBus);
+	}
 
-	@SuppressWarnings({ "deprecation", "unused" })
+	@SuppressWarnings({ "deprecation"})
 	@Override
 	public void run() {
 		
@@ -157,97 +170,4 @@ public class SimulationThread extends Thread {
 		return id;
 	}
 	
-	
-	private void test() {
-		
-		Date initialDate = project.getInitialDate();
-		Date lastDate = project.getFinalDate();
-		
-		ProbabilisticDistribution pd = new ProbabilisticDistribution();
-		pd.WeibullDistribution(1.55075, 601.44131);
-		
-		ProbabilisticDistribution passenger = new ProbabilisticDistribution();
-		passenger.WeibullDistribution(5,7);
-		
-		Queue<Date> passengersTime = new LinkedList<Date>();
-		Queue<Long> Hobsps = new LinkedList<Long>();
-		
-		// ==========================================
-		// simulation of passengers
-		// ==========================================
-		
-		int simulationTime =  (int) ((lastDate.getTime()-initialDate.getTime())/1000)/60;
-		//System.out.println("simulationTime "+simulationTime);
-		
-		for (int i = 1; i <= simulationTime+60; i++) {
-			
-			int numPassenger = (int) Math.round(passenger.getSample());
-			//System.out.println("numPassenger "+numPassenger);
-			
-			for (int j = 0; j < numPassenger; j++) {
-				Date passengerArrivetime = new Date(initialDate.getTime() + (i * 60 * 1000));
-				passengersTime.offer(passengerArrivetime);
-				//System.out.println(time.toGMTString());
-			}
-			
-		}
-		
-		while (initialDate.getTime() < lastDate.getTime()) {
-
-			// ==========================================
-			// simulation of first station
-			// ==========================================
-
-			Event simulationEvents[] = (Event[]) eventGenerator.generateFirstStation(project,pd,pd);
-			SimulationEvent arrive = (SimulationEvent) simulationEvents[0];
-			SimulationEvent leave = (SimulationEvent) simulationEvents[1];
-			events.add(arrive);
-			events.add(leave);
-			
-			
-			Date passengerArrivetime = passengersTime.poll();
-			int numPassengersPerBus = 0;
-			
-			while(!passengersTime.isEmpty() && passengerArrivetime.getTime()<=leave.getDate().getTime()) {
-				numPassengersPerBus ++;
-				long Hobsp = (leave.getDate().getTime() - passengerArrivetime.getTime())/1000;
-				Hobsps.add(Hobsp);
-				passengerArrivetime = passengersTime.poll();
-			}
-			
-			System.out.println("Passengers per Bus "+numPassengersPerBus);
-			
-			
-			System.out.println("O: " + arrive.getDate().toGMTString()+" Buses " + arrive.getBusId());
-			System.out.println("X: " + leave.getDate().toGMTString()+" Buses " + leave.getBusId());
-			System.out.println(" ");
-			
-			project.setInitialDate(arrive.getDate());
-			project.setNextDate(leave.getDate());
-			initialDate = leave.getDate();
-			
-
-			// ==========================================
-			// simulation of next stations
-			// ==========================================
-			
-			String tab = "";
-			
-			for (int i = 0; i < 2; i++) {
-				
-				tab += "	";
-				
-				simulationEvents = (Event[]) eventGenerator.generateNextStation(leave,pd,pd);
-				arrive = (SimulationEvent) simulationEvents[0];
-				leave = (SimulationEvent) simulationEvents[1];
-				events.add(arrive);
-				events.add(leave);
-				
-				System.out.println(tab + "O: " + arrive.getDate().toGMTString()+" Buses " + arrive.getBusId());
-				System.out.println(tab + "X: " + leave.getDate().toGMTString()+" Buses " + leave.getBusId());
-				System.out.println(" ");
-			}
-		}
-//		allEvents();
-	}
 }
--- simulationFrameworkDT/src/main/java/com/simulationFrameworkDT/simulation/event/eventProvider/EventGenerator.java
+++ simulationFrameworkDT/src/main/java/com/simulationFrameworkDT/simulation/event/eventProvider/EventGenerator.java
@@ -6,64 +6,9 @@ import java.util.Queue;
 
 import com.simulationFrameworkDT.model.SimulationEvent;
 import com.simulationFrameworkDT.simulation.event.Event;
-import com.simulationFrameworkDT.simulation.state.Project;
 import com.simulationFrameworkDT.simulation.tools.IDistribution;
 
 public class EventGenerator {
-	
-	public Event[] generateFirstStation(Project project, IDistribution ai, IDistribution si){
-		
-		Date initialDate = project.getInitialDate();
-		Date nextDate = project.getNextDate();
-
-		int interArrivalTime = (int) ai.getSample(); //calculate the intern arrival time with the distribution
-		int serviceTime = (int) si.getSample(); //calculate the service time with the distribution
-		
-		long initialTime = initialDate.getTime();
-		long lastService = nextDate.getTime();
-		
-		long arriveTime = initialTime + (interArrivalTime*1000); // calculate the arrive time
-		Date arrivetDate = new Date(arriveTime);
-		
-		//if current time less than last service time the current time change to last service
-		if(arriveTime < lastService) {
-			arriveTime = lastService;
-		}
-		
-		long leaveTime = arriveTime + (serviceTime*1000); //calculate the moment which the bus leave the station
-		Date leaveDate = new Date(leaveTime); 
-		
-		
-		long id = 00;//generate the bus id
-		SimulationEvent arrive = new SimulationEvent(true,id,500250, 0, arrivetDate);//create the event for arrive
-		SimulationEvent leave  = new SimulationEvent(false,id,500250, 0, leaveDate);// create the event for leave
-		Event events [] = {arrive,leave};
-		
-		return events;
-	}
-	
-	public Event[] generateNextStation(SimulationEvent simulationEvent, IDistribution ai, IDistribution si){
-		
-		Date initialDate = simulationEvent.getDate();
-
-		int interArrivalTime = (int) ai.getSample(); //calculate the intern arrival time with the distribution
-		int serviceTime = (int) si.getSample(); //calculate the service time with the distribution
-		
-		long initialTime = initialDate.getTime();
-		
-		long arriveTime = initialTime + (interArrivalTime*1000); // calculate the arrive time
-		Date arrivetDate = new Date(arriveTime);
-		
-		long leaveTime = arriveTime + (serviceTime*1000); //calculate the moment which the bus leave the station
-		Date leaveDate = new Date(leaveTime); 
-		
-		long id = simulationEvent.getBusId();//generate the bus id
-		SimulationEvent arrive = new SimulationEvent(true,id,500250, 0, arrivetDate);//create the event for arrive
-		SimulationEvent leave  = new SimulationEvent(false,id,500250, 0, leaveDate);// create the event for leave
-		Event events [] = {arrive,leave};
-		
-		return events;
-	}
 	
 	public Event generateAi(Date date, IDistribution ai, long id){
 		long initialTime = date.getTime();
